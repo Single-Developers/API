dd9699ca5270fc1d3c48dc37eb2ccd50878e1764
Malith Rukshan <85282650+Malith-Rukshan@users.noreply.github.com>
2021-12-27 11:33:49 +0530

    🔥 SSH Creator API 🔥

---
+++ ssh.py
@@ -0,0 +1,54 @@
+import requests
+import json
+
+servers=json.loads(requests.get('https://single-developers.herokuapp.com/servers').content)
+for server in servers:
+    id=str(server)
+    ip=servers[str(server)]['ip']
+    location=servers[str(server)]['location']
+    emoji=servers[str(server)]['emoji']
+    print(
+f"""◇ Server ID : {id}
+◇ Server Host : {ip}
+◇ Server Location : {emoji} {location} {emoji}
+""")
+serverid=input('Server ID : ')
+
+server=json.loads(requests.get(f'https://single-developers.herokuapp.com/servers?id={serverid}').content)
+status=requests.get(f'https://single-developers.herokuapp.com/servers?status={serverid}').content
+ip=server['ip']
+location=server['location']
+emoji=server['emoji']
+print(
+f"""
+◇ Server ID : {serverid}
+◇ Server Host : {ip}
+◇ Server Location : {emoji} {location} {emoji}
+◇ Server Status : {str(status)}"""
+)
+
+username=input('User Name : ')
+password=input('Password : ')
+
+ssh=serverid+'$'+username+'$'+password
+ssh_result=requests.get(f'https://single-developers.herokuapp.com/create?ssh={str(ssh)}').content
+try:
+    json_ssh=json.loads(ssh_result)
+    user_name=json_ssh['username']
+    passwd=json_ssh['password']
+    port=json_ssh['port']
+    ex_date=json_ssh['ex_date']
+    login=json_ssh['login']
+    print(
+f"""
+◇ Server Location : {emoji} {location} {emoji}
+◇ Server Host : {ip}
+◇ SSL Port : {port}
+◇ User Name : {user_name}
+◇ Password : {passwd}
+◇ Expire Date : {ex_date}
+◇ Login : {login}
+<  https://t.me/SingleDevelopers  />"""
+)
+except:
+    print(ssh_result)
