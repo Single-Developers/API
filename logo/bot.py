bd0378ad7a5bfebaf94d303b8200a0f0bb211338
Malith Rukshan <85282650+Malith-Rukshan@users.noreply.github.com>
2022-01-13 00:33:14 +0530

    🐉 Python Telegram BOT
    Domain Updated  : )
    
    api.single-developers.software

--- logo/bot.py
+++ logo/bot.py
@@ -5,7 +5,7 @@ BOT_Token="7853451df5:SDSGHghjgjdsfgtyY0PXugoowhNL03vyI7w"
 updater = Updater(BOT_Token)
 
 def incoming_message_action(update, context):
-  image_url=requests.get('https://single-developers.herokuapp.com/logo?name='+(update.message.text).replace(' ','%20'))
+  image_url=requests.get('https://api.single-developers.software/logo?name='+(update.message.text).replace(' ','%20'))
   context.bot.sendPhoto(chat_id=update.message.chat.id, photo=image_url,
                           reply_to_message_id=update.message.reply_to_message.message_id)
 
