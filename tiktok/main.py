cb15e7b5216644b85c88082832eacad4163d54a1
Malith Rukshan <85282650+Malith-Rukshan@users.noreply.github.com>
2022-01-28 11:47:55 +0530

    📚 Simple Example 📚

---
+++ tiktok/main.py
@@ -0,0 +1,19 @@
+import requests
+
+API = 'https://api.single-developers.software/tiktok?url='
+
+req = requests.get(API+input('◇ TikTok URL :- '))
+
+print('◇ Downloading No Watermark Video....')
+
+with open("no_watermark.mp4", "wb") as handle:
+    for data in requests.get(req['no_watermark'], stream=True).iter_content():
+        handle.write(data)
+        
+print('◇ Downloading Watermark Video....')
+     
+with open("watermark.mp4", "wb") as handle:
+    for data in requests.get(req['watermark'], stream=True).iter_content():
+        handle.write(data)
+
+print('◇ Download Completed !!!')
